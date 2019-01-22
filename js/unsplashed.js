88424e08bd8de6c2885f537376ab465bf3d52ed4
crisstauter <crisstauter@yahoo.com>
2019-01-22 12:09:08 -0500

    Added
    Added Unsplashed

---
+++ js/unsplashed.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    //feed to parse
+    //var feed = "https://api.unsplash.com/users/crisstauter/collections/3858845?client_id=67554b521283b042dbf1ca2fb73f5f783ab523a571ee81d44dcd0a18ff368bfb";
+    var feed = "https://api.unsplash.com/collections/3858845/photos?client_id=67554b521283b042dbf1ca2fb73f5f783ab523a571ee81d44dcd0a18ff368bfb";
+ 
+    $.ajax(feed, {
+ 
+        dataType:"json",
+        success:function(data) {
+            console.log(data);
+            //console.log(data[0].preview_photos[0].urls.full);
+            $(data).each(function () {
+                console.log($(this)[0].description);
+                console.log($(this)[0].urls.full);
+ 
+            });
+ 
+        }   
+    }); 
+});
