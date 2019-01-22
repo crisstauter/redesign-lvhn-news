795bf8c4a3d6df82ac1f5a6fa3f710f964d6e8e9
crisstauter <crisstauter@yahoo.com>
2019-01-22 15:25:59 -0500

    update
    update home code

---
+++ js/header.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	//feed to parse
+	var feed = "https://news.lvhn.org/tagfeed/en-us/tags/blog";
+	$.ajax(feed, {
+		accepts:{
+			xml:"application/rss+xml"
+		},
+		dataType:"xml",
+		success:function(data) {
+			$(data).find("item").each(function (index) { 
+        if (index > 0) {
+          return false;
+        }
+				var el = $(this);
+        var myblogheader = "<div class='container'><div class='col-sm-12 col-md-12 col-lg-12'>";
+        myblogheader += "<img class='blog_image' alt='' src='" + el.find("pp\\:image" ).text() + "'/>";
+
+        myblogheader += "<div class='header_blog_text'><a href='"+ el.find("link").text()+"'><h2 class='header_blog_title'>" + el.find("title").text() +"</h2></a>";
+        myblogheader += "<div class='header_blog_date'><p>" + moment(el.find("pubDate").text()).format("MMMM Do YYYY") +"</p>";
+        //removed description because HTML might get mangled. SHould use Summary with text
+       // myblogpost += "<div class=''>" + el.find("description").text().substring(0, 100) +"</div>";
+        
+        myblogheader += "</div></div>";
+        $(header_post).append(myblogheader);
+        
+			});
+		}	
+	});	
+});
